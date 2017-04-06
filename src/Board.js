758826771c28a87dd268cd596679c93101579f87
Hack Reactor Student <student@hackreactor.com>
2017-04-05 17:11:12 -0700

    Implement methods for checking row/column/diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -73,18 +73,34 @@
     /*=========================================================================
     =                 TODO: fill in these Helper Functions                    =
     =========================================================================*/
-
+    // sum the contents of a particular .get() row
+    sumRow: function(rowIndex){
+      return this.get(rowIndex).reduce(
+        function(retVal,elem){
+          retVal += elem;
+          return retVal;
+        },0);
+    },
     // ROWS - run from left to right
     // --------------------------------------------------------------
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      if(this.sumRow(rowIndex) > 1) {
+        return true;
+      }
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var rows = this.rows();
+      for(var i = 0; i < rows.length; i++) {
+        if(this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -92,14 +108,33 @@
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
+    // sum the contents of a particular column in the matrix
+    sumCol: function(colIndex){
+      return this.rows().reduce(
+        function(retVal,row){
+        //add the element at colindex to the retVal;
+          retVal += row[colIndex];
+          return retVal;
+      },0);
+    },
+
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      if (this.sumCol(colIndex)>1){ 
+        return true;
+      }
+      return false;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var numOfColumns = this.get(0).length;  // should really be based on 'n' but I guess that's not defined in this scope? 
+      for (var i = 0; i < numOfColumns; i++){
+        if (this.hasColConflictAt(i)){
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -109,12 +144,37 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var matrix = this.rows();
+      var limit = ( matrix.length ); // really should be 'n'
+      var checkCol = majorDiagonalColumnIndexAtFirstRow; // Starting Column
+      var checkRow = 0; // Starts at top
+      var sum = 0;
+      var mirrorSum = 0;
+
+      while ( (checkRow < limit) && (checkCol < limit) ){
+        var mirrorRow = checkCol;
+        var mirrorCol = checkRow
+        sum += matrix[checkRow][checkCol];
+        mirrorSum += matrix[mirrorRow][mirrorCol];
+        
+        if (sum > 1 || mirrorSum > 1){ return true; }
+
+        checkCol++;
+        checkRow++;
+      }
+      //otherwise...
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var numOfColumns = this.get(0).length;  // should really be based on 'n' but I guess that's not defined in this scope? 
+      for (var i = 0; i < numOfColumns; i++){
+        if (this.hasMajorDiagonalConflictAt(i)){
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -124,12 +184,38 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var matrix = this.rows();
+      var limit = ( matrix.length ); // really should be 'n'
+      var trueLimit = limit-1;
+      var checkCol = minorDiagonalColumnIndexAtFirstRow; // Starting Column
+      var checkRow = 0; 
+      var sum = 0;
+      var mirrorSum = 0;
+
+      while ( (checkRow < limit ) && (checkCol >= 0) ){
+        var mirrorRow = trueLimit - checkCol;
+        var mirrorCol = trueLimit - checkRow;
+        sum += matrix[checkRow][checkCol];
+        mirrorSum += matrix[mirrorRow][mirrorCol];
+        
+        if (sum > 1 || mirrorSum > 1){ return true; }
+
+        checkCol--;
+        checkRow++;
+      }
+      //otherwise...
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var limit = this.rows().length;  // should really be based on 'n' but I guess that's not defined in this scope? 
+      for (var i = 0; i < limit; i++){
+        if (this.hasMinorDiagonalConflictAt(i)){
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
@@ -146,3 +232,22 @@
   };
 
 }());
+
+
+
+/* NOTES 
+
+Board.rows() = returns all rows
+Board.get(x) = returns the single row x (0-indexed)
+
+var rookBoard = new Board([ [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1] ]);
+var workingBoard = new Board([ [0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0], [0, 0, 1, 0] ]);
+var badBoard = new Board([ [1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1] ]);
+var threeByBoard = new Board ([ [1,0,0] , [0,1,0] , [0,0,1]]);
+var badThreeByBoard = new Board ([ [1,0,0] , [1,0,0] , [1,0,0]]);
+
+var blankBoard = new Board([ [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ]);
+
+var badMajDiagBoard = new Board([ [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0] ]);
+var badMinDiagBoard = new Board([ [0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0] ]);
+*/
